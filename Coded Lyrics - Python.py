e0e64a5132e6ce89e2a24c657a948dc264a9b1f3
Sharif <64508248+sharifgt@users.noreply.github.com>
2022-05-26 21:46:29 -0400

    Freestyle 01 Files
    1st Draft

---
+++ Coded Lyrics - Python.py
@@ -0,0 +1,12 @@
+# Lyrically Tech Freestyle 01
+# Artist: 'Rif
+# Instrumental: '93 'Til Infinity by Souls of Mischief
+
+print("\n")
+lyrics_file = open("Plain Text Lyrics.txt",'r')
+
+for line in lyrics_file:
+    print(line.strip())
+
+lyrics_file.close()
+print("\n")
